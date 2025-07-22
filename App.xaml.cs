26a15895744d4de097f7002273115803afd0b564
Tbread <itaebread@gmail.com>
2025-07-22 16:49:15 +0900

    Initial Commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,27 @@ namespace Book
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            LoginWindow login = new LoginWindow();
+            bool? result = login.ShowDialog();
+            if (result == true)
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
+            else
+            {
+                Shutdown();
+            }
+        }
+
+        public static void LaunchGitHub()
+        {
+            String url = "https://github.com/tbread";
+            var processInfo = new System.Diagnostics.ProcessStartInfo(url);
+            System.Diagnostics.Process.Start(processInfo);
+        }
     }
 }
--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -10,18 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 
 namespace Book
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// LoginWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class LoginWindow
     {
-        public MainWindow()
+        public LoginWindow()
         {
             InitializeComponent();
         }
