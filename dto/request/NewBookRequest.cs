cc45a306f2873161111902fba6823a7c8a347630
Tbread <itaebread@gmail.com>
2025-07-30 01:20:17 +0900

    Feat: Add dto for create new book request

---
+++ dto/request/NewBookRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Book.enums;
+
+namespace Book.dto.request
+{
+    public class NewBookRequest
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public string isbn { get; set; }
+        public string isni { get; set; }
+        public KDC classificationNumber { get; set; }
+        public bool isSeries { get; set; }
+        public long? seriesId { get; set; }
+
+        public NewBookRequest(string title, string author, string publisher, string isbn, string isni, KDC classificationNumber, bool isSeries, long? seriesId)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.isbn = isbn;
+            this.isni = isni;
+            this.classificationNumber = classificationNumber;
+            this.isSeries = isSeries;
+            this.seriesId = seriesId;
+        }
+    }
+}
